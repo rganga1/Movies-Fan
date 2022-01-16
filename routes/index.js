82a060e70e55f4a5a54051c8a214e0722c6043a0
Rakesh <r@gmail.com>
2022-01-16 19:00:46 +0530

    Individual movie page added

--- routes/index.js
+++ routes/index.js
@@ -16,10 +16,22 @@ router.get("/", function (req, res, next) {
   request.get(nowPlayingUrl, (error, response, movieData) => {
     const parsedData = JSON.parse(movieData);
     // res.json(parsedData); //install jsonview in chrome to see movie objects in json
-    res.render("index", { parsedData: parsedData.results }); 
+    res.render("index", { parsedData: parsedData.results });
     console.log(parsedData.results[1]);
   });
 });
 
+router.get("/movie/:id", (req, res, next) => {
+  const movieId = req.params.id;
+  // res.json(movieId);
+  const thisMovieUrl = `${apiBaseUrl}/movie/${movieId}?api_key=${apiKey}`;
+  // res.send(thisMovieUrl); //checked
+  request.get(thisMovieUrl, (error, response, movieData) => {
+    const parsedData = JSON.parse(movieData);
+    res.render("single-movie", {
+      parsedData: parsedData,
+    });
+  });
+});
 
 module.exports = router;

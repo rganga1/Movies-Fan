adc31fce85e4dd8b45af5ecddb250e24e8cf28bb
Rakesh <r@gmail.com>
2022-01-16 18:20:07 +0530

    ALl movie images

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,25 @@
 var express = require("express");
 var router = express.Router();
-const request=require('request');
+const request = require("request");
 
 const apiKey = "1fb720b97cc13e580c2c35e1138f90f8";
 const apiBaseUrl = "http://api.themoviedb.org/3";
 const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`;
 const imageBaseUrl = "http://image.tmdb.org/t/p/w300";
 
+router.use((req,res,next)=>{
+  res.locals.imageBaseUrl=imageBaseUrl;
+})
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  request.get(nowPlayingUrl,(error,response,movieData)=>{
-    const parsedData=JSON.parse(movieData);
-    res.json(parsedData); //install jsonview in chrome to see movie objects in json
-  })
-  // res.render("index", { title: "Express" });
+  request.get(nowPlayingUrl, (error, response, movieData) => {
+    const parsedData = JSON.parse(movieData);
+    // res.json(parsedData); //install jsonview in chrome to see movie objects in json
+    res.render("index", { parsedData: parsedData.results }); 
+    console.log(parsedData.results[1]);
+  });
 });
+
 
 module.exports = router;

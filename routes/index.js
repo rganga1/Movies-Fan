61c8bb37a012140d133eb6e6e3d355e0114186be
Rakesh <--global>
2022-01-17 20:24:47 +0530

    Resolved single page

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,102 @@
 var express = require("express");
 var router = express.Router();
+
+// Import request module to support HTTP requests:
+
 const request = require("request");
 
-const apiKey = "1fb720b97cc13e580c2c35e1138f90f8";
+// Set up API Key:
+
+const apiKey = "d1923f949a5469388df50e0f61d9bdfb";
+
+// Configure API:
+
 const apiBaseUrl = "http://api.themoviedb.org/3";
+
 const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`;
+
 const imageBaseUrl = "http://image.tmdb.org/t/p/w300";
 
+// Set up path to postage images:
+
 router.use((req, res, next) => {
-  res.locals.imageBaseUrl = "http://image.tmdb.org/t/p/w300";
-  next(); //This is must - all problems started without this
+  res.locals.imageBaseUrl = imageBaseUrl;
+  next();
 });
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
+  // Send HTTP request:
+
+  // request takes 2 arguments:
+  //   1. It takes the URL for the get operation.
+  //   2. The callback, which takes 3 arguments, to run on completion of the HTTP request:
+  //     a. error (if any)
+  //     b. http response
+  //     c. JSON data the server sent back
+
   request.get(nowPlayingUrl, (error, response, movieData) => {
+    // FOR DIAGNOSTIC PURPOSES ONLY:
+
+    // console.log("======The Error======");
+    // console.log(error);
+    // console.log("======The Response======");
+    // console.log(response);
+
+    // ACTIVE CODE:
+
     const parsedData = JSON.parse(movieData);
-    // res.json(parsedData); //install jsonview in chrome to see movie objects in json
-    res.render("index", { parsedData: parsedData.results });
-    console.log(parsedData.results[1]);
+
+    // FOR DIAGNOSTIC PURPOSES ONLY:
+
+    // console.log(parsedData);
+
+    // ACTIVE CODE:
+
+    // res.json(parsedData); //  <-- Outputs JSON format data to browser.
+
+    res.render("index", {
+      parsedData: parsedData.results,
+    });
   });
 });
 
+//  Get detail page:
+
+// NOTE: '/movie/:id'is a parameterized, or wildcard route.
+//       That means that ':id' is going to be stored in
+//       res.params.
+
 router.get("/movie/:id", (req, res, next) => {
-  const movieId = req.params.id;
-  // res.json(movieId);
+  // FOR DIAGNOSTIC PURPOSES ONLY:
+
+  // res.json(req.params.id);
+
+  // ACTIVE CODE:
+
+  const movieId = req.params.id; // <-- id for path
+
+  // Piece together URL for movie detail view:
+
   const thisMovieUrl = `${apiBaseUrl}/movie/${movieId}?api_key=${apiKey}`;
-  // res.send(thisMovieUrl); //checked
+
+  // FOR DIAGNOSTIC PURPOSES ONLY:
+
+  // res.send(thisMovieUrl);
+
+  // ACTIVE CODE:
+
   request.get(thisMovieUrl, (error, response, movieData) => {
+    // Set up parsed data from HTTP request:
+
     const parsedData = JSON.parse(movieData);
+
+    // Render returned data:
+
     res.render("single-movie", {
-      parsedData: parsedData,
+      parsedData,
     });
-  });
+  }); // end request.get(thisMovieMurl)
 });
 
 module.exports = router;
